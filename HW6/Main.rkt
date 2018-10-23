c4394366e4b9f67493c2dfaf03121b3adc600054
gfvandehei <gfvandehei@gmail.com>
2018-10-22 21:54:24 -0400

    changed the way things are parsed, am having trouple running functions now though

--- HW6/Main.rkt
+++ HW6/Main.rkt
@@ -1,46 +1,58 @@
 
-(define prog
+(define (prog expr) 
   ;prog pulls out the first element in a list (a terminal) and sends the two nonterminals following
   ;to the associated evaluation functions
-  (lambda (expr)
-    ;expr a list representing a boolean expression
-    (cond((eqv? expr #t) #t)
-         ((eqv? expr #f) #f)
-         (else (let ()
-                 (define args (cdr expr))
-                 ;if this is mylet assign the variable and call the function
-                 (car(expr) car(args) cdr(args))
-                 (display "else")
-                 )
+  ;expr a list representing a boolean expression
+  (display expr)
+  (newline)
+  (cond((equal? expr 'true) #t)
+       ((equal? expr 'false) #f)
+       (else
+              ;the command will be (car expr))
+              (display (car expr))
+              (newline)
+              (display (car(cdr expr)))
+              (let ((cmd (car expr))
+                    )
+                (cmd (car(cdr expr)) (car(cdr expr)))
                 )
-                 
-         )
-    )
-  )
+              ;((car expr) #t #t)
+             )
+   )
+)
+  ;)
 
-(define false #f)
+(define false #f) ;defines false to its boolean value
 
-(define true #t)
+(define true #t) ;defines true to its boolean value
 
 (define myignore
+  ;the myignore function will return false for the expression passed
   (lambda(val)
     #f
     )
   )
 
-(define myor
-  (lambda (lval rval)
-    (or (prog lval) (prog rval))
-    )
+(define (myor lval rval)
+  ;lval, the left expression
+  ;rval, the right expression
+  ;the myor function will return the equivilant of the expression (lval or rval)
+  (display "or")
+  (or (prog lval) (prog rval))
   )
 
 (define myand
+  ;lval, the left expression to be passed to and
+  ;rval, the right expression to be oassed to and
+  ;myand function, will return the equivilant of (lval and rval)
   (lambda (lval rval)
     (and (prog lval) (prog rval))
     )
   )
 
 (define mynot
+  ;val, and boolean expression
+  ;mynot function, returns the opposite boolean of the val expression
   (lambda (val)
     (not prog(val))
     )
@@ -48,20 +60,25 @@
 
 (define mylet
   (lambda (var expr1 expr2)
-    (display var)
+    (display "HELLO")
     )
   )
-  
-(define myinterpreter '(
-    (prog true)
-    (prog (myor (myand true (myignore (myor true false))) (myand true false)))
-    (prog (mylet z (myor false true) (myand z true)))
-    (prog (mylet a true (myor (mylet b (myand true false) (myor false b)) (myand false a))))
-    (prog (mylet x true (myor (mylet x (myand true false) (myand true x)) (myand true x))))
-))
+
+(define (runcommand cmd)
+  (define commandcd (car cmd))
+  (define exp1 (car(cdr cmd)))
+  (prog exp1)
+  )
 
 ;; YOUR CODE HERE
 (define (myinterpreter x)
 ;;
-  (x)
-)
+  (cond ((eqv? x '()) (display x))
+        (else(
+             (display (runcommand (car x)))
+             (newline)
+             (myinterpreter (cdr x))
+             )
+        )
+  )
+ )
