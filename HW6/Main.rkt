87678c3ced4ae505fc1d519f77f30d77166c54e6
gfvandehei <gfvandehei@gmail.com>
2018-10-23 11:00:23 -0400

    all the returned data is correct, must now add to output to list

--- HW6/Main.rkt
+++ HW6/Main.rkt
@@ -1,5 +1,5 @@
 
-(define (prog expr) 
+(define (prog expr varlist) 
   ;prog pulls out the first element in a list (a terminal) and sends the two nonterminals following
   ;to the associated evaluation functions
   ;expr a list representing a boolean expression
@@ -8,20 +8,20 @@
   (newline)
   (cond((equal? expr 'true) #t)
        ((equal? expr 'false) #f)
-       ((symbol? expr) #f)
+       ((symbol? expr) (varlook expr varlist))
        (else
               ;the command will be (car expr))
               (cond ((equal? (car expr) 'myor)
-                  (myor (car(cdr expr)) (car(cdr(cdr expr))))
+                  (myor (car(cdr expr)) (car(cdr(cdr expr))) varlist)
                   )
               ((equal? (car expr) 'myand)
-                  (myand (car(cdr expr)) (car(cdr(cdr expr))))
+                  (myand (car(cdr expr)) (car(cdr(cdr expr))) varlist)
                   )
               ((equal? (car expr) 'myignore)
                   (myignore (car(cdr expr)))
                   )
               ((equal? (car expr) 'mylet)
-                  (mylet (car(cdr expr)) (car(cdr(cdr expr))) (car(cdr(cdr(cdr expr)))))
+                  (mylet (car(cdr expr)) (car(cdr(cdr expr))) (car(cdr(cdr(cdr expr)))) varlist)
                   )
               (else #f)
               )
@@ -29,6 +29,18 @@
    )
 )
   ;)
+
+(define (varlook var varlist)
+  (display "VARLOOK: ")
+  (display var)
+  (newline)
+  (define curvar (car varlist))
+  (cond ((equal? curvar var) (car(cdr varlist)))
+        (else
+         (varlook var (cdr varlist))
+         )
+        )
+  )
 
 (define false #f) ;defines false to its boolean value
 
@@ -39,7 +51,7 @@
   #f
   )
 
-(define (myor lval rval)
+(define (myor lval rval varlist)
   ;lval, the left expression
   ;rval, the right expression
   ;the myor function will return the equivilant of the expression (lval or rval)
@@ -50,28 +62,29 @@
   (display rval)
   (newline)
   (newline)
-  (cond ((and ( or (equal? lval 'true) (equal? lval 'false)) ( or (equal? rval 'true) (equal? rval 'false)))
-         (or (prog lval) (prog rval))
-         )
-        ((or (equal? lval 'true) (equal? lval 'false))
-         ;(or (prog lval) (prog (car rval)))
-         (or (prog lval) (prog rval))
-         (display "RVAL OR")
-         (newline)
-         )
-        ((or (equal? rval 'true) (equal? rval 'false))
-         (or (prog lval) (prog rval))
-         (display "LVAL OR")
-         (newline)
-         )
-        (else
-             (or (prog lval) (prog rval))
-             )
-        )
+  ;(cond ((and ( or (equal? lval 'true) (equal? lval 'false)) ( or (equal? rval 'true) (equal? rval 'false)))
+   ;      (or (prog lval varlist) (prog rval varlist))
+    ;     )
+     ;   ((or (equal? lval 'true) (equal? lval 'false))
+      ;   ;(or (prog lval) (prog (car rval)))
+       ;  (or (prog lval varlist) (prog rval varlist))
+        ; (display "RVAL OR")
+         ;(newline)
+         ;)
+        ;((or (equal? rval 'true) (equal? rval 'false))
+  ;       (or (prog lval varlist) (prog rval varlist))
+   ;      (display "LVAL OR")
+    ;     (newline)
+     ;    )
+      ;  (else
+       ;      (or (prog lval varlist) (prog rval varlist))
+        ;     )
+        ;)
+  (or (prog lval varlist) (prog rval varlist))
   
   )
 
-(define (myand lval rval)
+(define (myand lval rval varlist)
   ;lval, the left expression to be passed to and
   ;rval, the right expression to be oassed to and
   ;myand function, will return the equivilant of (lval and rval)
@@ -82,22 +95,21 @@
   (display rval)
   (newline)
   (newline)
-  (cond ((and ( or (equal? lval 'true) (equal? lval 'false)) ( or (equal? rval 'true) (equal? rval 'false))) (and (prog lval) (prog rval)))
-        ((or (equal? lval 'true) (equal? lval 'false)) (and (prog lval) (prog rval)))
-        ((or (equal? rval 'true) (equal? rval 'false)) (and (prog lval) (prog rval)))
-        (else (and (prog lval) (prog rval)))
-        )
+  ;(cond ((and ( or (equal? lval 'true) (equal? lval 'false)) ( or (equal? rval 'true) (equal? rval 'false))) (and (prog lval) (prog rval)))
+   ;     ((or (equal? lval 'true) (equal? lval 'false)) (and (prog lval) (prog rval)))
+    ;    ((or (equal? rval 'true) (equal? rval 'false)) (and (prog lval) (prog rval)))
+     ;   (else (and (prog lval) (prog rval)))
+      ;  )
+  (and (prog lval varlist) (prog rval varlist))
   )
 
-(define mynot
+(define (mynot varlist)
   ;val, and boolean expression
   ;mynot function, returns the opposite boolean of the val expression
-  (lambda (val)
-    (not prog(val))
-    )
+  (not prog(val varlist))
   )
 
-(define (mylet var expr1 expr2)
+(define (mylet var expr1 expr2 varlist)
   (display "mylet")
   (newline)
   (display expr1)
@@ -105,19 +117,23 @@
   (display expr2)
   (newline)
   (newline)
-  (define var (prog expr1))
-  (prog expr2)
+  ;make a pair of variable and value
+  ;add to varlist
+  (define varlist2 (cons var (cons(prog expr1 varlist) varlist)))
+  (prog expr2 varlist2)
   )
 
 (define (runcommand cmd)
+  (define varlist '())
   (define commandcd (car cmd))
   (define exp1 (car(cdr cmd)))
-  (prog exp1)
+  (prog exp1 varlist)
   )
   
 ;; YOUR CODE HERE
 (define (myinterpreter x)
 ;;define var list
+  (define anslist '())
   (cond ((eqv? x '()) (display x))
         (else(
              (display (runcommand (car x)))
@@ -126,4 +142,5 @@
              )
         )
   )
+  anslist
  )
